31fd81ab3034eb1503cc2b9eed9ce6b16d8e2606
teohyc2018@gmail.com <you@example.com>
2026-02-11 11:27:36 +0800

    rag db

---
+++ RAG_pneumonia_agent.py
@@ -0,0 +1,20 @@
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+PERSIST_DIR = "vectordb"
+COLLECTION_NAME = "chest_xray_knowledge"
+
+embeddings = OllamaEmbeddings(
+    model="embeddinggemma:latest"
+)
+
+vectorstore = Chroma(
+    persist_directory=PERSIST_DIR,
+    collection_name=COLLECTION_NAME,
+    embedding_function=embeddings
+)
+
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 7}
+)
---
+++ build_vectordb.py
@@ -0,0 +1,44 @@
+import os
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+
+DOCS_PATH = "rag_docs"
+PERSIST_DIR = "vectordb"
+COLLECTION_NAME = "chest_xray_knowledge"
+
+def build_vector_db():
+    documents = []
+
+    for file in os.listdir(DOCS_PATH): #presenting all files in the directory
+        if file.endswith(".txt"):
+            loader = TextLoader(os.path.join(DOCS_PATH, file))
+            documents.extend(loader.load())
+
+    print(f"Loaded {len(documents)} documents.")
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 200,
+        chunk_overlap = 50
+    )
+
+    chunks = splitter.split_documents(documents)
+    print(f"Split into {len(chunks)} chunks.")
+
+    embeddings = OllamaEmbeddings(
+        model="embeddinggemma:latest"
+    )
+
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=PERSIST_DIR,
+        collection_name=COLLECTION_NAME
+    )
+
+    vectorstore.persist()
+    print("Vector database built and persisted.")
+
+if __name__ == "__main__":
+    build_vector_db()
